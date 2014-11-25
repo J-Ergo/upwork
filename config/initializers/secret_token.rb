edc3b318b0a0b59d37fbbed0c2cd81dd6402c496
vagrant <vagrant@rails-dev-box>
2014-11-25 16:45:32 +0000

    Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Upwork::Application.config.secret_key_base = '02f53ed401d8aa4a48170d4baa892979f6f17e3e4fdbc97b9e18e117c93042a137c4e4c00b692efc69afa8fe83c5b3fb976ec1dc9d90b465315d6e98b81bc66f'
